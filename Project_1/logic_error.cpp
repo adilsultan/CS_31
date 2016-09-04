6a6dbfe11de4b666e4f71c5754637f77ce514ba6
Adil Sultan <adilsultan916@gmail.com>
2016-09-03 18:23:00 -0700

    Project 1 files
    Uploaded all the files for project 1 of CS31

---
+++ Project_1/logic_error.cpp
@@ -0,0 +1,34 @@
+// Code for Project 1
+// Report poll results
+
+#include <iostream>
+using namespace std;   // pp. 38-39 in Savitch 6/e explains this line
+
+int main()
+{
+    int numberSurveyed;
+    int forClinders;
+    int forCrump;
+    
+    cout << "How many registered voters were surveyed? ";
+    cin >> numberSurveyed;
+    cout << "How many of them say they will vote for Clinders? ";
+    cin >> forClinders;
+    cout << "How many of them say they will vote for Crump? ";
+    cin >> forCrump;
+    
+    double pctClinders = 10.0 * forClinders / numberSurveyed;
+    double pctCrump = 100.0 * forCrump / numberSurveyed;
+    
+    cout.setf(ios::fixed);       // see pp. 32-33 in Savitch 6/e
+    cout.precision(1);
+    
+    cout << endl;
+    cout << pctClinders << "% say they will vote for Clinders." << endl;
+    cout << pctCrump << "% say they will vote for Crump." << endl;
+    
+    if (forClinders > forCrump)
+        cout << "Clinders is predicted to win the election." << endl;
+    else
+        cout << "Crump is predicted to win the election." << endl;
+}
